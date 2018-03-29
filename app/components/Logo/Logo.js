4efcaecebe6752a9e8f75df00a7fa0c7afaa1cb9
Haris Shahid <harisshahid01@yahoo.com>
2018-03-29 09:47:58 +0500

    animation started but stuck in debugging it show an error

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { View, Text, ImageBackground, Image } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text, ImageBackground, Image, Animated, Keyboard } from 'react-native';
 import styles from './style';
 
-const Logo = () => (
-    <View style={styles.container} >
-        <ImageBackground resizeMode="contain" style={styles.containerImage} source={require('./images/bg.png')} >
-            <Image resizeMode="contain"   style={styles.image} source={require('./images/Logo.png')} />
-        </ImageBackground>
-        <Text style={styles.text} >Currency Converter</Text>
-    </View>
-);
+class Logo extends Component{
+    componentDidMount(){
+        this.keyboardShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardShow)
+        this.keyboardHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardHide)
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    keyboardShow = () => {
+
+    }
+
+    keyboardHide = () => {
+
+    }
+
+    render(){
+        return (
+            <View style={styles.container} >
+                <ImageBackground resizeMode="contain" style={styles.containerImage} source={require('./images/bg.png')} >
+                    <Image resizeMode="contain"   style={styles.image} source={require('./images/Logo.png')} />
+                </ImageBackground>
+                <Text style={styles.text} >Currency Converter</Text>
+            </View>
+        );
+    }
+} 
 
 export default Logo;
