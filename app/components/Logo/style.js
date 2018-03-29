2f147c98cf4eb69aefef811f2f01d81621b83de9
Haris Shahid <harisshahid01@yahoo.com>
2018-03-29 21:53:44 +0500

    animated logo completed

--- app/components/Logo/style.js
+++ app/components/Logo/style.js
@@ -1,13 +1,13 @@
-import EStyleSheet from 'react-native-extended-stylesheet';
 import { Dimensions } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-const imageWidth = Dimensions.get('window').width / 2 ;
+const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
-    $largeContainerSize: imageWidth,
-    $largeImageSize: imageWidth / 2,
     $smallContainerSize: imageWidth / 2,
     $smallImageSize: imageWidth / 4,
+    $largeContainerSize: imageWidth,
+    $largeImageSize: imageWidth / 2,
     container: {
         alignItems: 'center',
     },
@@ -17,14 +17,15 @@ export default EStyleSheet.create({
         width: '$largeContainerSize',
         height: '$largeContainerSize',
     },
-    image: {
+    logo: {
         width: '$largeImageSize',
+        tintColor: '$primaryBlue',
     },
     text: {
-        fontWeight: '600',
+        color: '$white',
         fontSize: 28,
         letterSpacing: -0.5,
         marginTop: 15,
-        color: '$white'
-    }
-})
+        fontWeight: '600',
+    },
+});
