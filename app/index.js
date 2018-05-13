ffd7c3ca68bcf0be92690f02755b8861371cf7b8
Haris Shahid <harisshahid01@yahoo.com>
2018-05-13 14:40:44 +0500

    store configure completed

--- app/index.js
+++ app/index.js
@@ -27,7 +27,7 @@ EStyleSheet.build({
 export default () => (
     <Provider store={store} >
         <AlertProvider>
-            <Navigator />
+            <Navigator onNavigationStateChange={null} />
         </AlertProvider>
     </Provider>
 )
