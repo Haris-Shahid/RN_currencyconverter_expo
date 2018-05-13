1e0a45283dd567d0a7960d61bd16a13cfaf88444
Haris Shahid <harisshahid01@yahoo.com>
2018-05-13 14:37:35 +0500

    add react logger

--- app/config/store.js
+++ app/config/store.js
@@ -1,4 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import reducers from '../reducers';
 
-export default createStore(reducers);
+const middleware = [];
+
+//checking only development is in process
+if( process.env.NODE_ENV === 'development' ){
+    middleware.push(logger);
+}
+
+export default createStore(reducers, applyMiddleware(...middleware));
