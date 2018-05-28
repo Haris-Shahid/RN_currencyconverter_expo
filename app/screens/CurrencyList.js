7192c4fc333de2d065f9801490d8b42bfb40dac5
Haris Shahid <harisshahid01@yahoo.com>
2018-05-28 17:17:34 +0500

    Reading Currency Data from store throught mapstatetoprops

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -12,29 +12,35 @@ class CurrencyList extends Component {
     static propTypes = {
         navigation: PropTypes.object,
         dispatch: PropTypes.func,
+        baseCurrency: PropTypes.string,
+        quoteCurrency: PropTypes.string,
     }
 
     handlePress = (currency) => {
-const { type } = this.props.navigation.state.params;
-if(type === 'base'){
-    //todo dispatch change base
-    this.props.dispatch(changeBaseCurrency(currency))
-}else if(type === 'quote'){
-    //todo dispatch chnage quote
-    this.props.dispatch(changeQuoteCurrency(currency))
-}
+        const { type } = this.props.navigation.state.params;
+        if (type === 'base') {
+            //todo dispatch change base
+            this.props.dispatch(changeBaseCurrency(currency))
+        } else if (type === 'quote') {
+            //todo dispatch chnage quote
+            this.props.dispatch(changeQuoteCurrency(currency))
+        }
 
         this.props.navigation.goBack(null)
     }
 
     render() {
+        let comparisonCurrency = this.props.baseCurrency;
+        if (this.props.navigation.state.params.type === 'quote') {
+            comparisonCurrency = this.props.quoteCurrency;
+        }
         return (
             <View style={{ flex: 1 }} >
                 <StatusBar barStyle="default" translucent={false} />
                 <FlatList
                     data={currencies}
                     renderItem={({ item }) => <ListItem text={item}
-                        selected={item === TEMP_CURRENT_CURRENCY}
+                        selected={item === comparisonCurrency}
                         onPress={() => this.handlePress(item)}
                     />}
                     keyExtractor={(item) => item}
@@ -45,4 +51,9 @@ if(type === 'base'){
     }
 }
 
-export default connect()(CurrencyList);
+const mapStateToProps = (state) => ({
+    baseCurrency: state.currencies.baseCurrency,
+    quoteCurrency: state.currencies.quoteCurrency
+})
+
+export default connect(mapStateToProps)(CurrencyList);
